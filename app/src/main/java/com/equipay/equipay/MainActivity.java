5f6e0da7e4acf565b1a16fe27d257f34456b4c89
Rohan Gupta <rg16feb@gmail.com>
2022-05-19 00:37:28 +0530

    Adding Splash Screen to the App

--- app/src/main/java/com/equipay/equipay/MainActivity.java
+++ app/src/main/java/com/equipay/equipay/MainActivity.java
@@ -3,6 +3,7 @@ package com.equipay.equipay;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -14,14 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createSessionBtn = findViewById(R.id.createSessionBtn);
-        createSessionBtn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OccasionsActivity.class);
-                startActivity(intent);
-                finish();
+            public void run() {
+                startActivity(new Intent(MainActivity.this,OccasionsActivity.class));
             }
-        });
+        },1000);
+
+
+//        createSessionBtn = findViewById(R.id.createSessionBtn);
+//        createSessionBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, OccasionsActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
